5c0db58654525976c8fafeb7c721977bde881379
shani384 <118974759+shani384@users.noreply.github.com>
2023-10-04 10:48:23 +0300

    start

---
+++ AdminClient/src/component/login/LoginPageController.java
@@ -0,0 +1,105 @@
+package component.login;
+
+import com.sun.istack.internal.NotNull;
+import component.mainapp.AppMainController;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.Response;
+import util.http.Constants;
+import util.http.HttpClientUtil;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class LoginPageController {
+    @FXML
+    public Label LabelLoginPage;
+    public Label LabelWelcome;
+    @FXML
+    public TextField TextUserName;
+    @FXML
+    public Button ButtonLogin;
+    @FXML
+    public Label errorMessageLabel;
+    //private ChatAppMainController chatAppMainController;
+    private final StringProperty errorMessageProperty = new SimpleStringProperty();
+    private AppMainController appMainController;
+
+    @FXML
+    public void initialize() {
+        errorMessageLabel.textProperty().bind(errorMessageProperty);
+//        HttpClientUtil.setCookieManagerLoggingFacility(line ->
+//                Platform.runLater(() ->
+//                        updateHttpStatusLine(line)));
+    }
+
+    @FXML
+    private void loginButtonClicked(ActionEvent event) {
+
+        String userName = TextUserName.getText();
+        if (userName.isEmpty()) {
+            errorMessageProperty.set("User name is empty.\n You can't login with empty user name");
+            return;
+        }
+        //noinspection ConstantConditions
+        String finalUrl = HttpUrl
+                .parse(Constants.LOGIN_PAGE)
+                .newBuilder()
+                .addQueryParameter("username", userName)
+                .build()
+                .toString();
+
+        //updateHttpStatusLine("New request is launched for: " + finalUrl);
+        // TODO: 04/10/2023  
+        HttpClientUtil.runAsync(finalUrl, new Callback() {
+
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Platform.runLater(() ->
+                        errorMessageProperty.set("Something went wrong: " + e.getMessage())
+                );
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if (response.code() != 200) {
+                    String responseBody = response.body().string();
+                    Platform.runLater(() ->
+                            errorMessageProperty.set("Something went wrong: " + responseBody)
+                    );
+                } else {
+                    Platform.runLater(() -> {
+                        appMainController.updateUserName(userName);
+                        //AppMainController.switchToChatRoom();
+                    });
+                }
+            }
+        });
+    }
+
+        @FXML
+        private void userNameKeyTyped(KeyEvent event) {
+            errorMessageProperty.set("");
+        }
+
+        @FXML
+        private void quitButtonClicked(ActionEvent e) {
+            Platform.exit();
+        }
+
+//        private void updateHttpStatusLine(String data) {
+//            AppMainController.updateHttpLine(data);
+//        }
+
+        public void setAppMainController(AppMainController chatAppMainController) {
+            this.appMainController = chatAppMainController;
+        }
+    }
---
+++ AdminClient/src/main/AdminClient.java
@@ -0,0 +1,51 @@
+package main;
+
+import component.mainapp.AppMainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import util.http.HttpClientUtil;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class AdminClient extends Application {
+
+    private static final String MAIN_PAGE_FXML_RESOURCE_LOCATION = "/component/mainapp/mainPanelView.fxml";
+    private AppMainController chatAppMainController;
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setMinHeight(600);
+        primaryStage.setMinWidth(600);
+        primaryStage.setTitle("Chat App Client");
+
+        URL loginPage = getClass().getResource(MAIN_PAGE_FXML_RESOURCE_LOCATION);
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(loginPage);
+            Parent root = fxmlLoader.load();
+            chatAppMainController = fxmlLoader.getController();
+
+            Scene scene = new Scene(root, 700, 600);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+        HttpClientUtil.shutdown();
+        chatAppMainController.close();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
